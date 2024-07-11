01dbabb85fe75bfd6407cb83c5458a6e46b99f60
shimada-yuji <shishishishishimama@gmail.com>
2024-07-11 16:29:57 +0900

    1

---
+++ kadai_008/Price_Chapter08.java
@@ -0,0 +1,15 @@
+package text.section_05;
+
+public class Price_Chapter08 {
+	public static void main(String[] args) {
+		int userAge = 30;
+		int serviceCost =;
+		
+		serviceCost = switch(userAge) {
+		case 10 -> 1000;
+		case 20 -> 2000;
+		case 30
+		};
+	}
+
+}
