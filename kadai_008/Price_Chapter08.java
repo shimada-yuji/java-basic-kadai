30f765cc62609d39f0d001b1fecfe978e89a390f
shimada-yuji <shishishishishimama@gmail.com>
2024-07-11 20:25:37 +0900

    1

--- kadai_008/Price_Chapter08.java
+++ kadai_008/Price_Chapter08.java
@@ -3,13 +3,22 @@ package text.section_05;
 public class Price_Chapter08 {
 	public static void main(String[] args) {
 		int userAge = 30;
-		int serviceCost =;
+		int serviceCost;
 		
 		serviceCost = switch(userAge) {
 		case 10 -> 1000;
 		case 20 -> 2000;
-		case 30
+		case 30 -> 3000;
+		case 40 -> 3000;
+		case 50 -> 4000;
+		case 60 -> 4000;
+		case 70 -> 4000;
+		case 80 -> 5000;
+		default -> 500;
 		};
+		
+		System.out.println(userAge+"代の料金は"+serviceCost+"円");
+		
 	}
 
 }
