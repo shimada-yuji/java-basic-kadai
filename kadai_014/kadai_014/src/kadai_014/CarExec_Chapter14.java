2bd70ecc81a6105bf5c48770fb3888a1cb1ea42e
shimada-yuji <shishishishishimama@gmail.com>
2024-07-17 14:54:54 +0900

    1

---
+++ kadai_014/kadai_014/src/kadai_014/CarExec_Chapter14.java
@@ -0,0 +1,12 @@
+package kadai_014;
+
+public class CarExec_Chapter14 {
+	public static void main(String[] args) {
+		Car_Chapter14 gear = new Car_Chapter14();
+		
+		gear.gearChange(3);
+		gear.run();
+	}
+	
+
+}
---
+++ kadai_014/kadai_014/src/kadai_014/Car_Chapter14.java
@@ -0,0 +1,24 @@
+package kadai_014;
+
+public class Car_Chapter14 {
+	private int gear = 1;//ギアを表す
+	private int speed = 10;//速度を表す
+	
+	public void gearChange(final int afterGear) {
+		this.gear=afterGear;
+		switch(this.gear) {
+		case 1 ->this.speed = 10;
+		case 2 ->this.speed = 20;
+		case 3 ->this.speed = 30;
+		case 4 ->this.speed = 40;
+		case 5 ->this.speed = 50;
+		default ->this.speed = 10;
+		}
+		
+	}
+	public void run() {
+		System.out.println("ギア1から"+this.gear+"に切り替えました");
+		System.out.println("速度は時速"+this.speed+"kmです");
+	}
+
+}
