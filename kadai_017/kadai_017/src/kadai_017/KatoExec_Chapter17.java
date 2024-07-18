1e2ee98ae8787d10f5ff34a1d677d3d740de888d
shimada-yuji <shishishishishimama@gmail.com>
2024-07-18 21:13:58 +0900

    1

---
+++ kadai_017/kadai_017/src/kadai_017/KatoExec_Chapter17.java
@@ -0,0 +1,17 @@
+package kadai_017;
+
+public class KatoExec_Chapter17{
+	public static void main(String[] main) {
+		KatoTaro_Chapter17 taro=new KatoTaro_Chapter17() ;
+		taro.setGivenName();
+		taro.execIntroduce();
+		
+		KatoIchiro_Chapter17 ichiro=new KatoIchiro_Chapter17() ;
+		ichiro.setGivenName();
+		ichiro.execIntroduce();
+		
+		KatoHanako_Chapter17 hanako=new KatoHanako_Chapter17() ;
+		hanako.setGivenName();
+		hanako.execIntroduce();
+		}
+}
