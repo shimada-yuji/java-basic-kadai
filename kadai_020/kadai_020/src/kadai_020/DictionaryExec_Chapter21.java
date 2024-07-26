d2bd31288b8aa8d47104025414b9dad1ee7a0972
shimada-yuji <shishishishishimama@gmail.com>
2024-07-26 16:51:59 +0900

    １

--- kadai_020/kadai_020/src/kadai_020/DictionaryExec_Chapter21.java
+++ kadai_020/kadai_020/src/kadai_020/DictionaryExec_Chapter21.java
@@ -4,11 +4,10 @@ public class DictionaryExec_Chapter21 {
 	public static void main(String[] args) {
 		Dictionary_Chapter20 dictionary=new Dictionary_Chapter20() ;
 		String[] tanngo= {"apple","banana","grape","orange"};
-		dictionary.jisyo();
-		for(int i=0;i<=tanngo.length;i++) {
-			System.out.println(tanngo[i]);
+		
+		
 		}
 	
     }
 
-}
+
