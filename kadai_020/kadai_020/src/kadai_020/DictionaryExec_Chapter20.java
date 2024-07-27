321aa88115d546c7c079ab95999105a0be3d3ae3
shimada-yuji <shishishishishimama@gmail.com>
2024-07-27 11:58:53 +0900

    1

--- kadai_020/kadai_020/src/kadai_020/DictionaryExec_Chapter20.java
+++ kadai_020/kadai_020/src/kadai_020/DictionaryExec_Chapter20.java
@@ -1,6 +1,6 @@
 package kadai_020;
 
-public class DictionaryExec_Chapter21 {
+public class DictionaryExec_Chapter20 {
 	public static void main(String[] args) {
 		Dictionary_Chapter20 dictionary=new Dictionary_Chapter20() ;
 		String[] tanngo= {"apple","banana","grape","orange"};
